23e0653fde17804db2f291b2ee2813ee56e4cdd5
kriskata007 <kristian.kostadinov-26v@mgberon.com>
2025-02-20 18:50:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace chvhbhg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+        {
+            new Student("Maksim",5.32),
+            new Student("Daniel", 5.87),
+            new Student("Jivko", 5.57),
+            new Student("Ivan", 5.87)
+        };
+            InsertionSortByGradeAndName(students);
+            Console.WriteLine("Sorted List: ");
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        public static List<Student> InsertionSortByGradeAndName(List<Student> students)
+        {
+            for (int i = 1; i < students.Count; i++)
+            {
+                Student current = students[i];
+                int j = i - 1;
+
+                
+                while (j >= 0 && (students[j].AverageGrade > current.AverageGrade ||
+                                  (students[j].AverageGrade == current.AverageGrade && string.Compare(students[j].Name, current.Name) > 0)))
+                {
+                    students[j + 1] = students[j];
+                    j--;
+                }
+
+                students[j + 1] = current;
+            }
+
+            return students;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chvhbhg
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public double AverageGrade { get; set; }
+
+        public Student(string name, double averageGrade)
+        {
+            Name = name;
+            AverageGrade = averageGrade;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Average Grade: {AverageGrade}";
+        }
+    }
+}
